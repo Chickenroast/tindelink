7920ee61a0f649622f4c29d727c4c659d88acfd4
Julia Michel T. <juliamicheltex@gmail.com>
2024-02-13 19:03:55 +0100

    saves

--- components/auth-components.tsx
+++ components/auth-components.tsx
@@ -1,5 +1,20 @@
-import { signIn, signOut } from "auth"
-import { Button } from "./ui/button"
+import { signIn, signOut } from "auth";
+import { Button } from "./ui/button";
+
+// components/signup.tsx (or any other appropriate file)
+import prisma from "../lib/prisma";
+
+async function createUser(email: string, name?: string) {
+  return await prisma.user.create({
+    data: {
+      email,
+      name,
+    },
+  });
+}
+
+// Usage
+createUser("user@example.com", "John Doe");
 
 export function SignIn({
   provider,
@@ -8,21 +23,21 @@ export function SignIn({
   return (
     <form
       action={async () => {
-        "use server"
-        await signIn(provider)
+        "use server";
+        await signIn(provider);
       }}
     >
       <Button {...props}>Sign In</Button>
     </form>
-  )
+  );
 }
 
 export function SignOut(props: React.ComponentPropsWithRef<typeof Button>) {
   return (
     <form
       action={async () => {
-        "use server"
-        await signOut()
+        "use server";
+        await signOut();
       }}
       className="w-full"
     >
@@ -30,5 +45,5 @@ export function SignOut(props: React.ComponentPropsWithRef<typeof Button>) {
         Sign Out
       </Button>
     </form>
-  )
+  );
 }
